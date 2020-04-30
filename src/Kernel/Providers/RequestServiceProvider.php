43d358e05807c9188d3713fc14ff762f285c11d0
王 <513972422@qq.com>
2020-04-30 10:54:49 +0800

    添加版本号?

--- src/Kernel/Providers/RequestServiceProvider.php
+++ src/Kernel/Providers/RequestServiceProvider.php
@@ -3,7 +3,23 @@
 namespace HyPerfEasyWeChat\Kernel\Providers;
 
 
+use Pimple\Container;
+use Symfony\Component\HttpFoundation\Request;
+
 class RequestServiceProvider
 {
-
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $pimple A container instance
+     */
+    public function register(Container $pimple)
+    {
+        $pimple['request'] = function () {
+            return Request::createFromGlobals();
+        };
+    }
 }
